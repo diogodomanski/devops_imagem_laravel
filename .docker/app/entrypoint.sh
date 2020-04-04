45f689e2158c802583695bd743ebfe1af2c8a12c
Diogo Domanski de Souza <diogo.domanski@gmail.com>
2020-04-04 14:37:41 -0300

    Added Dockerfile.prod

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Wait until DB is up and running
-dockerize -template ./.docker/app/.env:.env -wait tcp://app_db:3306 -timeout 40s
-
 cd /var/www
 
 # Install project dependencies
@@ -10,7 +7,6 @@ composer install
 
 ## Run start scripts for laravel
 php artisan key:generate
-php artisan config:cache
 php artisan migrate
 
 # Start php-fpm
